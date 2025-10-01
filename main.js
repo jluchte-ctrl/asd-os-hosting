5639da22c2b00cfd46b8ac8d0daa5d8dac21548a
jluchte-ctrl <jluchte@gmail.com>
2025-10-01 14:44:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-import app from './firebaseconfig.js';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB3ab9_jZiAYB1-ql_6IX2XsDVXD2UpCv8",
+  authDomain: "gen-lang-client-0814041184.firebaseapp.com",
+  projectId: "gen-lang-client-0814041184",
+  storageBucket: "gen-lang-client-0814041184.appspot.com",
+  messagingSenderId: "519555891397",
+  appId: "1:519555891397:web:09f875db43f04cd12f30ac",
+  measurementId: "G-8YT1LKFV3T"
+};
+
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
