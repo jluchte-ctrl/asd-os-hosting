1c1767ce4254f499c5cdf00d8366ed3fe03c840e
jluchte-ctrl <jluchte@gmail.com>
2025-10-01 14:20:40 -0600

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCe64LxHzYt_XP0UpYPwRek6JH5QVWEkrg",
+  apiKey: "AIzaSyB3ab9_jZiAYB1-ql_6IX2XsDVXD2UpCv8",
   authDomain: "gen-lang-client-0814041184.firebaseapp.com",
   projectId: "gen-lang-client-0814041184",
   storageBucket: "gen-lang-client-0814041184.appspot.com",
   messagingSenderId: "519555891397",
-  appId: "1:519555891397:web:034bafe8be45e6832f30ac"
+  appId: "1:519555891397:web:09f875db43f04cd12f30ac",
+  measurementId: "G-8YT1LKFV3T"
 };
 
 const app = initializeApp(firebaseConfig);
